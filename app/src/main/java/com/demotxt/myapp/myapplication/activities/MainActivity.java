7f4de791da83c4e1776d5740483d3ac8e3e76125
Francisndungu2 <francianmbugua@gmail.com>
2021-01-25 21:44:41 +0300

    addSignupform

--- app/src/main/java/com/demotxt/myapp/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/demotxt/myapp/myapplication/activities/MainActivity.java
@@ -1,9 +1,14 @@
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,3 +36,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameText.getText().toString();
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+                if(TextUtils.isEmpty(email) ||TextUtils.isEmpty(name) || TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Enter every details", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length() < 6){
+                    Toast.makeText(MainActivity.this, "Password must be 6 characters", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(MainActivity.this, Datadd.class));
+                                    }
+                                    else {
+                                        Toast.makeText(MainActivity.this, "Couldn't Create an account", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+    }
+}
