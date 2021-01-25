1896ce2501e5174ed5cec5ee637c191040939fc4
Francisndungu2 <francianmbugua@gmail.com>
2021-01-25 21:37:07 +0300

    addclasstrack

--- app/src/main/java/com/demotxt/myapp/myapplication/activities/LoginActivity.java
+++ app/src/main/java/com/demotxt/myapp/myapplication/activities/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.demotxt.myapp.myapplication;
+package com.demotxt.myapp.myapplication.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.demotxt.myapp.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
