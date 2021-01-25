c5809236fc23c58c029bd89a1b1dbeee1010d48b
Francisndungu2 <francianmbugua@gmail.com>
2021-01-25 21:42:45 +0300

    addhomebuttons

--- app/src/main/java/com/demotxt/myapp/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/demotxt/myapp/myapplication/activities/MainActivity.java
@@ -1,13 +1,33 @@
-package com.demotxt.myapp.myapplication.activities;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText nameText, emailText, passwordText;
+    Button signupBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
-}
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        nameText = findViewById(R.id.name);
+        emailText = findViewById(R.id.email);
+        passwordText = findViewById(R.id.password);
+        signupBtn = findViewById(R.id.signupBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
